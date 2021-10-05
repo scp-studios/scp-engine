48d92c64446f61ef22283724e1a36d8990a69e5c
Liangyue Zhao <liangyuez1@outlook.com>
2021-10-05 10:21:20 -0400

    Created the project skeleton.

---
+++ src/scp-engine/dummy-source.cpp
@@ -0,0 +1,17 @@
+void hello()
+{
+    // Hello! This is a file created merely for the purpose of getting things 
+    // setted up. Don't worry about this.
+    
+    int nengLi = 0;
+    for (unsigned int i = 0; i < 80; i++)
+    {
+        nengLi = nengLi + i;
+    }
+    
+    ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+    
+    const char* yo = "Hello!";
+    const char* no = "No way!";
+    const char* hoohoo = "Hello, this is a test!";
+}
