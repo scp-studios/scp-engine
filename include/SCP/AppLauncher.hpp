0cc09a19bcb162a7346412e005c5ef122fb1037c
Liangyue Zhao <liangyuez1@outlook.com>
2021-10-07 12:22:34 -0400

    Added a function to launch the Application.

---
+++ include/SCP/AppLauncher.hpp
@@ -0,0 +1,36 @@
+#ifndef D7BB7F5C_3B43_4A7E_9DCF_E7A16BB70115
+#define D7BB7F5C_3B43_4A7E_9DCF_E7A16BB70115
+
+#include <stdexcept>
+#include <iostream>
+
+namespace SCP
+{
+    class Application;
+    
+    namespace AppLauncher
+    {
+        // Simple function that basically just lauches an Application.
+        template<typename AppClass>
+        int launchApplication()
+        {
+            try
+            {
+                AppClass app;
+                while (app.isOpen())
+                {
+                    app.update();
+                    app.render();
+                }
+            } catch (std::runtime_error& e)
+            {
+                std::cerr << "[FATAL ERROR]: " << e.what() << std::endl;
+                return -1;
+            }
+            
+            return 0;
+        }
+    }
+}
+
+#endif /* D7BB7F5C_3B43_4A7E_9DCF_E7A16BB70115 */
