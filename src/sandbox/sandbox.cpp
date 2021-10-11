e4a94909a2b409070339fae90c004f46cdb2830c
Liangyue Zhao <liangyuez1@outlook.com>
2021-10-11 14:20:17 -0400

    Used the new launching system.

--- src/sandbox/sandbox.cpp
+++ src/sandbox/sandbox.cpp
@@ -1,48 +1,5 @@
-#include <SCP/Application.hpp>
 #include <SCP/AppLauncher.hpp>
 
-using SCP::Application;
-namespace AppLauncher = SCP::AppLauncher;
+#include "TestScene.hpp"
 
-class Sandbox: public SCP::Application
-{
-public:
-    Sandbox()
-    {
-        
-    }
-    
-    
-    
-    
-    void update() override
-    {
-        
-    }
-    
-    
-    
-    
-    void render() override
-    {
-        
-    }
-    
-    
-    
-    
-    bool isOpen() override
-    {
-        return true;
-    }
-    
-    
-    
-    
-    ~Sandbox()
-    {
-        
-    }
-};
-
-SCP_LAUNCH_APPLICATION(Sandbox)
+SCP_LAUNCH_APPLICATION(TestScene)
