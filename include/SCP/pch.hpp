6a70fade781a4743a6190691e43965edf10ec7ab
Liangyue Zhao <liangyuez1@outlook.com>
2021-10-11 16:59:50 -0400

    Added some headers to the precompiled header.

--- include/SCP/pch.hpp
+++ include/SCP/pch.hpp
@@ -1,6 +1,8 @@
 #ifndef A9784BED_1688_44A7_999D_264ED7ED09D9
 #define A9784BED_1688_44A7_999D_264ED7ED09D9
 
-
+#include <iostream>
+#include <vector>
+#include <queue>
 
 #endif /* A9784BED_1688_44A7_999D_264ED7ED09D9 */
