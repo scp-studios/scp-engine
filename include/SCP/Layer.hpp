3f13c61e900b69b650224a3b6454e6fdbaaa9a21
Liangyue Zhao <liangyuez1@outlook.com>
2021-10-11 17:08:15 -0400

    created the header for a basic event dispatcher.

---
+++ include/SCP/Layer.hpp
@@ -0,0 +1,10 @@
+#ifndef FA08338D_60A0_43B6_8665_8118B5EB6E01
+#define FA08338D_60A0_43B6_8665_8118B5EB6E01
+
+namespace SCP
+{
+    // This class is a stub so far.
+    class Layer {}
+}
+
+#endif /* FA08338D_60A0_43B6_8665_8118B5EB6E01 */
