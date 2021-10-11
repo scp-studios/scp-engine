17987e25428c5d9a7cf391a2e160a8dc6d071127
Liangyue Zhao <liangyuez1@outlook.com>
2021-10-11 14:03:34 -0400

    Created a basic scene class.

--- include/SCP/Scene.hpp
+++ include/SCP/Scene.hpp
@@ -5,7 +5,52 @@ namespace SCP
 {
     class Scene
     {
+    public:
+        // Update the scene.
+        virtual void update() = 0;
         
+        // Render the scene.
+        virtual void render() = 0;
+        
+        // Update the active scene
+        static void updateActive() { active->update(); }
+        
+        // Render the active scene
+        static void renderActive() { active->render(); }
+        
+        // Set the active scene.
+        template<typename T>
+        static void setActive()
+        {
+            if (active != nullptr)
+            {
+                delete active;
+            }
+            
+            active = new T();
+        }
+        
+        
+        
+        
+        
+        // Remove the active scene
+        static void removeActive()
+        {
+            if (active != nullptr)
+            {
+                delete active;
+            }
+            
+            active = nullptr;
+        }
+        
+        // Destroys the scene.
+        virtual ~Scene() {}
+        
+    private:
+        // The active scene
+        static Scene* active;
     };
 }
 
