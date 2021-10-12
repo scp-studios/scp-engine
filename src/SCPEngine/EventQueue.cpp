58a578717ac81cb6580455f0e4b1f59cdf30af6e
Liangyue Zhao <liangyuez1@outlook.com>
2021-10-12 17:32:51 -0400

    Implemented the event queue.

---
+++ src/SCPEngine/EventQueue.cpp
@@ -0,0 +1,43 @@
+#include <SCP/EventQueue.hpp>
+
+using SCP::EventQueue;
+
+EventQueue& EventQueue::getInstance()
+{
+    static EventQueue eventQueue;
+    return eventQueue;
+}
+
+
+
+
+void EventQueue::dispatch(Layer& layer)
+{
+    for (uint16_t i = 0; i < m_queue.size(); i++)
+    {
+        bool handled = false;
+        Events::Event& event = m_queue.front();
+        
+        switch (event.getType())
+        {
+        case Events::EventType::MOUSE_POS:
+            if (layer.onMousePosEvent(static_cast<Events::MousePosEvent&>(event)))
+            {
+                m_queue.pop();
+            }
+            break;
+        case Events::EventType::KEY:
+            if (layer.onKeyEvent(static_cast<Events::KeyEvent&>(event)))
+            {
+                m_queue.pop();
+            }
+            break;
+        case Events::EventType::MOUSE_CLICK:
+            if (layer.onMouseClickEvent(static_cast<Events::MouseClickEvent&>(event)))
+            {
+                m_queue.pop();
+            }
+            break;
+        }
+    }
+}
