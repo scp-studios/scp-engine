4d158c337913fab2d4b1332ca83e584cacfda639
Liangyue Zhao <liangyuez1@outlook.com>
2021-10-12 17:18:36 -0400

    Created the layer class.

--- include/SCP/Layer.hpp
+++ include/SCP/Layer.hpp
@@ -1,10 +1,30 @@
 #ifndef FA08338D_60A0_43B6_8665_8118B5EB6E01
 #define FA08338D_60A0_43B6_8665_8118B5EB6E01
 
+#include <SCP/Events/KeyEvent.hpp>
+#include <SCP/Events/MouseClickEvent.hpp>
+#include <SCP/Events/MousePosEvent.hpp>
+
 namespace SCP
 {
-    // This class is a stub so far.
-    class Layer {}
+    // A base class for a layer
+    class Layer {
+    public:
+        Layer() {}
+        
+        // Update
+        virtual void update() = 0;
+        
+        // Render
+        virtual void render() = 0;
+        
+        // Events
+        virtual bool onKeyEvent(const Events::KeyEvent& e);
+        virtual bool onMouseClickEvent(const Events::MouseClickEvent& e);
+        virtual bool onMousePosEvent(const Events::MousePosEvent& e);
+        
+        virtual ~Layer();
+    };
 }
 
 #endif /* FA08338D_60A0_43B6_8665_8118B5EB6E01 */
