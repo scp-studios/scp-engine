7a0ad2cac427d05a146596ca7598da039cd851af
Liangyue Zhao <liangyuez1@outlook.com>
2021-10-13 18:23:52 -0400

    Added an enumeration to represent the API.

---
+++ include/SCP/Graphics/API.hpp
@@ -0,0 +1,14 @@
+#ifndef A0C79A96_12A6_47A2_8C25_76213432FBEC
+#define A0C79A96_12A6_47A2_8C25_76213432FBEC
+
+namespace SCP::Graphics
+{
+    // An enumeration for the graphics API used by the engine. Currently,
+    // there's only OpenGL, but I'll add more later on.
+    enum class API
+    {
+        OpenGL
+    };
+}
+
+#endif /* A0C79A96_12A6_47A2_8C25_76213432FBEC */
