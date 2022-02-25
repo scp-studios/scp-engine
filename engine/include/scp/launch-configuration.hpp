9bf905e56e95e7ace3b92656740c68de2e9da82a
Liangyue "Yue" Zhao <liangyuezhao7@gmail.com>
2022-02-25 16:16:52 -0500

    Added a launch_configuration for future references.

---
+++ engine/include/scp/launch-configuration.hpp
@@ -0,0 +1,14 @@
+#ifndef E05C405F_C3EA_478D_ACED_FB9BABD38A85
+#define E05C405F_C3EA_478D_ACED_FB9BABD38A85
+
+namespace scp
+{
+    // A set of parameters for the game launcher as to how to run the game. It
+    // is empty for now because I haven't added any options yet.
+    struct launch_configuration_t
+    {
+        // Empty for now.
+    };
+}
+
+#endif /* E05C405F_C3EA_478D_ACED_FB9BABD38A85 */
