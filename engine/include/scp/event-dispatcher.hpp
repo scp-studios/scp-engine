1f7b0415fb1ead1b1a46421f2cb7a38821e75898
Liangyue "Yue" Zhao <liangyuezhao7@gmail.com>
2022-02-27 14:51:49 -0500

    Added a basic event dispatcher.

---
+++ engine/include/scp/event-dispatcher.hpp
@@ -0,0 +1,30 @@
+#ifndef CDC84E6A_90BB_4162_99DE_54E74478F92A
+#define CDC84E6A_90BB_4162_99DE_54E74478F92A
+
+#include <functional>
+
+namespace scp
+{
+    // Forward declarations.
+    class event_t;
+    
+    // No explanantions needed.
+    using event_handler_t = std::function<void(const event_t& p_event)>;
+    
+    // A simple event dispatcher for dispatching events. It abstracts the deta-
+    // ils of dispatching events away.
+    class event_dispatcher
+    {
+    public:
+        // Constructor.
+        event_dispatcher(event_handler_t handler): m_handler(handler) {}
+        
+        // Dispatch an event.
+        void dispatch(const event_t& event) const { m_handler(event); }
+    private:
+        // The function that is going to handle all the events.
+        event_handler_t m_handler;
+    };
+}
+
+#endif /* CDC84E6A_90BB_4162_99DE_54E74478F92A */
