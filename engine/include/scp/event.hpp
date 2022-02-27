a06c6d153ecd79c138eb736caec0e198bb2b6cf6
Liangyue "Yue" Zhao <liangyuezhao7@gmail.com>
2022-02-27 08:49:23 -0500

    Used the event type enumeration as the return type of get_type.

--- engine/include/scp/event.hpp
+++ engine/include/scp/event.hpp
@@ -5,6 +5,8 @@ typedef uint32_t;
 
 namespace scp
 {
+    enum class event_type_t;
+    
     // A generic event class. It is used within the event system. Every event 
     // type that can be dispatched must inherit this class in order for them to
     // work.
@@ -14,10 +16,8 @@ namespace scp
         // C++ will automatically insert a default constructor and all other c-
         // onstructors.
         
-        // Returns the type of the event. Used within the event dispatcher. Cu-
-        // rrently, we use an uint32_t to identify events, so that it's easier 
-        // to add more events. However, we may add an enumeration later on.
-        virtual uint32_t get_type() = 0;
+        // Returns the type of the event.
+        virtual event_type_t get_type() = 0;
     };
 }
 
