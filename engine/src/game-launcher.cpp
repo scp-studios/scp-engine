a2ddabdfd33c00a945e85a1745164590f4b52619
Liangyue " Yue <liangyuezhao7@gmail.com>
2022-03-12 17:50:35 -0500

    Display an error box on windows on fatal error.

--- engine/src/game-launcher.cpp
+++ engine/src/game-launcher.cpp
@@ -16,11 +16,16 @@ static void run()
     }
 }
 
-// System specific ways of handling fatal errors. For now, just output it to t-
-// he terminal.
+// System specific ways of handling fatal errors.
 static void handle_fatal_error(const scp::fatal_error_t& p_error)
 {
-    std::cerr << "[FATAL ERROR]: " << p_error.m_what << "\n";
+#ifdef SCP_PLATFORM_WINDOWS
+    wchar_t* error_message = nullptr;
+    mbstowcs(error_message, p_error.m_what.data(), p_error.m_what.size());
+    MessageBoxW(nullptr, error_message, L"FATAL ERROR", MB_ICONERROR | MB_OK);
+#else
+    std::cerr << "[FATAL ERROR]: " << p_error.m_what << std::endl;
+#endif
 }
 
 int scp::game_launcher::launch_game(const scp::launch_configuration_t& p_launch_configuration)
