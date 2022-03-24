eba3cb6e8f3101c45dfd837f4c8b106d63fe3f14
Liangyue "Yue" Zhao <liangyuezhao7@gmail.com>
2022-03-24 16:12:07 -0400

    Added a macro for marking arguments as explicitly unused.

--- engine/include/scp/scp.hpp
+++ engine/include/scp/scp.hpp
@@ -1,6 +1,8 @@
 #ifndef A855B898_4CA4_48BD_BB47_D005F2374B77
 #define A855B898_4CA4_48BD_BB47_D005F2374B77
 
+// A few things that everything will need.
+
 #ifdef SCP_ENGINE_BUILD_DLL
     #define SCPEXP __declspec(dllexport)
 #else
@@ -10,5 +12,8 @@
         #define SCPEXP
     #endif
 #endif
+
+// Macros for unused variables
+#define SCP_UNUSED_ARG(x) (void)x
 
 #endif /* A855B898_4CA4_48BD_BB47_D005F2374B77 */
