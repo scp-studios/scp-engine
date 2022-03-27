39ae372521506d98fea6a231066f311eeb02a66f
Liangyue "Yue" Zhao <liangyuezhao7@gmail.com>
2022-03-27 19:27:40 -0400

    Integrated the layer stack into the rest of scene class.

--- engine/src/scene.cpp
+++ engine/src/scene.cpp
@@ -53,23 +53,17 @@ void scene_t::on_active()
 
 void scene_t::update(double p_delta_time)
 {
-	// This is a function that runs every frame that may not run on the same t-
-	// hread as the render function. It is used to process things that may need
-	// to be processed every frame.
+	m_layer_stack.update(p_delta_time);
 }
 
 void scene_t::handle_event(const event_t& p_event)
 {
-	// Placeholder. Nothing here so far, but what it will do is it will propag-
-	// ate the event through the layer stack.
+	m_layer_stack.on_event(p_event);
 }
 
 void scene_t::render()
 {
-	// This is the function that will be called to render the scene. It will a-
-	// lways be called from the rendering thread whenever neccessary, though on
-	// some APIs that aren't specifically tied to any specific threads such as 
-	// Vulkan, this may not be called from that thread.
+	m_layer_stack.render();
 }
 
 scene_t* scene_t::active_scene = nullptr;
