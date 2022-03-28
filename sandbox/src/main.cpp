8b36d304705a568bf6254797d8f01b1e0b9b2852
Liangyue "Yue" Zhao <liangyuezhao7@gmail.com>
2022-03-28 17:58:39 -0400

    Added a simple test to test the layer stack alone.

--- sandbox/src/main.cpp
+++ sandbox/src/main.cpp
@@ -1,14 +1,43 @@
 #include <scp/game-launcher.hpp>
 #include <scp/launch-configuration.hpp>
 #include <scp/scene.hpp>
+#include <scp/layer-stack.hpp>
+#include <scp/layer.hpp>
+#include <iostream>
+
+class example_layer: public scp::layer_t
+{
+public:
+    void update(double p_delta_time) override
+    {
+        std::cout << "Updated!" << std::endl;
+    }
+    
+    ~example_layer()
+    {
+        
+    }
+};
+
+int old_main()
+{
+    scp::layer_stack_t layer_stack;
+    
+    layer_stack.push_layer<example_layer>();
+    
+    layer_stack.update(15);
+    layer_stack.render();
+    
+    return 0;
+}
 
 int main()
 {
-    scp::scene_t starting_scene;
+    scp::scene_t* starting_scene = new scp::scene_t();
     
     scp::launch_configuration_t launch_configuration = {
-        starting_scene
+        *starting_scene
     };
     
-    return scp::game_launcher::launch_game(launch_configuration);
+    scp::game_launcher::launch_game(launch_configuration);
 }
