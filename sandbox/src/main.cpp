675762654ef7952f6e5dd6f8eca895d07e8be3e6
Liangyue "Yue" Zhao <liangyuezhao7@gmail.com>
2022-03-31 17:29:00 -0400

    Removed unecessary code.

--- sandbox/src/main.cpp
+++ sandbox/src/main.cpp
@@ -1,35 +1,6 @@
 #include <scp/game-launcher.hpp>
 #include <scp/launch-configuration.hpp>
 #include <scp/scene.hpp>
-#include <scp/layer-stack.hpp>
-#include <scp/layer.hpp>
-#include <iostream>
-
-class example_layer: public scp::layer_t
-{
-public:
-    void update(double p_delta_time) override
-    {
-        std::cout << "Updated!" << std::endl;
-    }
-    
-    ~example_layer()
-    {
-        
-    }
-};
-
-int old_main()
-{
-    scp::layer_stack_t layer_stack;
-    
-    layer_stack.push_layer<example_layer>();
-    
-    layer_stack.update(15);
-    layer_stack.render();
-    
-    return 0;
-}
 
 int main()
 {
