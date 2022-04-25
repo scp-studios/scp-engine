a5c0c739d4ecb5956407e287531c2b8dd4d78ebe
Liangyue "Yue" Zhao <liangyuezhao7@gmail.com>
2022-04-25 17:57:08 -0400

    Added a enum for describing graphics APIs.

---
+++ engine/include/scp/graphics/api.hpp
@@ -0,0 +1,12 @@
+#ifndef EDE6888A_65B2_42C6_92EF_C8529BA6CBD4
+#define EDE6888A_65B2_42C6_92EF_C8529BA6CBD4
+
+namespace scp::graphics
+{
+    enum class api_e
+    {
+        opengl, vulkan, direct3d_11, direct3d_12, metal
+    }
+}
+
+#endif /* EDE6888A_65B2_42C6_92EF_C8529BA6CBD4 */
